9d455a890293b2b2b2a665f196cb2260c0c90725
Milad S <milad9890@gmail.com>
2023-09-27 22:21:39 -0400

    chore: add to prettier + add auth imgs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,7 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss"),require.resolve("@trivago/prettier-plugin-sort-imports")],
+  "importOrder": ["^react(.*)", "^next(.*)", "<THIRD_PARTY_MODULES>", "@/(.*)", "^[./]"],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true
+}
 
-module.exports = config;
