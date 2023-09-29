a197d69c3a864b4522b81e84752f8c66dbf01526
Milad S <milad9890@gmail.com>
2023-09-28 21:38:22 -0400

    merge issue fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
-const MyApp: AppType = ({
+const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps
+  pageProps: { session, ...pageProps },
 }) => {
   return (
+    <SessionProvider session={session}>
       <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
