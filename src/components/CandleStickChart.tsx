4c3955d5e32e2f085ada0097e389e366869fceb3
Milad S <milad9890@gmail.com>
2023-11-20 14:05:03 -0500

    feat:candle stick chart finished UI+Logic

---
+++ src/components/CandleStickChart.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+
+import { useRouter } from "next/router";
+
+import { type CandlestickData, generateMarkers } from "~/features/tradeDetails";
+import { api } from "~/utils/api";
+
+import { createChart } from "lightweight-charts";
+
+export const CandleStickChart = () => {
+  const router = useRouter();
+  const symbol = router.query.symbol as string;
+  const date = router.query.date as string;
+
+  const { data: details, isLoading } = api.trades.getTradeDetails.useQuery(
+    { symbol, date },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+  const { data } = api.trades.getTradesByDate.useQuery(
+    { symbol: symbol, date: date },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const { data: candleData = [] } = details ?? {};
+  const { dailyTrades } = data ?? {};
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const chart = createChart(chartRef.current!, {
+      layout: {
+        background: { color: "#161B26" },
+        textColor: "#FFF",
+      },
+      grid: {
+        vertLines: { color: "#444" },
+        horzLines: { color: "#444" },
+      },
+
+      width: 1000,
+      height: 715,
+
+      crosshair: {
+        mode: 0,
+        horzLine: {
+          labelBackgroundColor: "#FFF",
+        },
+      },
+
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+        borderColor: "#485158",
+      },
+    });
+
+    const candleSeries = chart.addCandlestickSeries();
+    candleSeries.applyOptions({
+      lastValueVisible: false,
+      priceLineVisible: false,
+    });
+
+    if (candleData && candleData.length > 0) {
+      const candleSeriesData: CandlestickData[] = candleData;
+      candleSeries.setData(candleSeriesData);
+    }
+
+    if (dailyTrades) candleSeries.setMarkers(generateMarkers({ dailyTrades }));
+
+    chart.timeScale().fitContent();
+
+    return () => {
+      chart.remove();
+    };
+  }, [candleData, dailyTrades]);
+
+  return (
+    <section className="relative">
+      <div
+        className={`cursor relative  ${isLoading && "opacity-20"}`}
+        ref={chartRef}
+        style={{ backgroundColor: "#624b4b" }}
+      ></div>
+      {isLoading && (
+        <span className="loading loading-infinity loading-lg absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-success"></span>
+      )}
+      {!dailyTrades ||
+        (dailyTrades.length === 0 && (
+          <div>
+            <span className="loading loading-dots loading-lg absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-success"></span>
+            <span className=" absolute left-1/2 top-1/2 z-10 mt-7 -translate-x-1/2 -translate-y-1/2 transform text-success">
+              No Data Available
+            </span>
+          </div>
+        ))}
+    </section>
+  );
+};
--- src/features/tradeDetails/utils/format.ts
+++ src/features/tradeDetails/utils/format.ts
@@ -1,7 +1,21 @@
-import { type UTCTimestamp } from "lightweight-charts";
+import type { AppRouter } from "~/server/api/root";
+
+import type { inferRouterOutputs } from "@trpc/server";
+
+import type {
+  SeriesMarkerPosition,
+  SeriesMarkerShape,
+  UTCTimestamp,
+} from "lightweight-charts";
 import moment from "moment";
 
+type RouterOutput = inferRouterOutputs<AppRouter>;
+type DailyTrades = RouterOutput["trades"]["getTradesByDate"];
+
 const MILLISECONDS = 1000;
+
+const GREEN_MARKER = "#4bAF59";
+const RED_MARKER = "#ff0419";
 
 export const getMarketTimes = (date: string) => {
   const marketOpenTime = "9:30:00";
@@ -30,3 +44,15 @@ export const timeToLocal = (originalTime: number) => {
 
 export const formatToUnix = (timestamp: string) =>
   moment(timestamp).startOf("minute").valueOf() / MILLISECONDS;
+
+export const generateMarkers = ({ dailyTrades }: DailyTrades) => {
+  return (
+    dailyTrades?.map((trade) => ({
+      time: trade.Marker as UTCTimestamp,
+      position: "aboveBar" as SeriesMarkerPosition,
+      color: trade.Volume > 0 ? GREEN_MARKER : RED_MARKER,
+      shape: "arrowDown" as SeriesMarkerShape,
+      text: `${trade.Volume} x ${trade.Price}`,
+    })) ?? []
+  );
+};
